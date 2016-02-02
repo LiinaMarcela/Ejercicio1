77b028a812d9f8104f110109d8eca06acc26d5c9
LiinaMarcela <lmbarrera35@misena.edu.co>
2016-02-01 21:35:44 -0500

    Hola Mundo

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+/**
+ *
+ * @author klf
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+}
